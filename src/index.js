654fae145b27e42a2c4236c672024b280a631406
Darius Pirvulescu <organicdarius@gmail.com>
2021-01-28 00:13:02 +0200

    Split firebase config code

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
-require("dotenv").config();
 const bodyParser = require("body-parser");
 const express = require("express");
-const firebase = require("firebase/app");
-require("firebase/auth");
-require("firebase/database");
+
+const auth = require('./firebase')
+const db = require('./db');
 
 const app = express();
-
-var firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_API_ID,
-};
-
-firebase.initializeApp(firebaseConfig);
-
-const auth = firebase.auth();
-const db = firebase.database();
 
 app.use(bodyParser.json());
 
