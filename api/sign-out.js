b1158928df9b0811afddb14e00be335b41efc13a
Darius Pirvulescu <organicdarius@gmail.com>
2021-01-28 02:53:45 +0200

    Fix sign-out function

--- api/sign-out.js
+++ api/sign-out.js
@@ -1,10 +1,21 @@
 const auth  = require("../lib/firebase");
 
 module.exports = (req, res) => {
-  auth
-    .signOut()
-    .then(() => res.send("User has been signed out"))
-    .catch(() => {
-      res.send("err");
-    });
+  let user = auth.currentUser
+
+  if (user) {
+    auth
+      .signOut()
+      .then(() => {
+        res.send({
+          message: "User has been signed out", 
+          user: user.email
+        })
+      })
+      .catch(() => {
+        res.send("err");
+      });
+  } else {
+    res.send("No user signed in");
+  }
 };
